9e2454d27304ea6d27f4757e9ec0ef00e8d3550a
Gregz9 <gregorkajda@gmail.com>
2022-09-19 23:10:10 +0200

    Screenshots of accuracy and loss over training and validation sets added

--- Image_Segmentation/train.py
+++ Image_Segmentation/train.py
@@ -201,8 +201,9 @@ def main(train_dir):
 
     train_data = get_train_data(indices[:272], train_batch_size)
     val_data = get_val_data(indices[272:], val_batch_size)
-    #model = segmentation_models.simple_model((HEIGHT, WIDTH, 3))
-    model = segmentation_models.unet((HEIGHT, WIDTH, 3))
+    model = segmentation_models.simple_model((HEIGHT, WIDTH, 3))
+    #model = segmentation_models.unet((HEIGHT, WIDTH, 3))
+    model.summary()
     loss_fn = get_loss_fn()
     metric_fn = preds_evaluated
     optimizer = get_optimizer()
